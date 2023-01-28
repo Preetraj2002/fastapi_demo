a806c752c0620611947b2ea4bb8403d616d858f6
Preetraj <preetrajgupta2002@gmail.com>
2023-01-28 22:11:23 +0530

    models for db created

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+# This file contains the user defined model or schema
+# i.e. the structure of the database (sqlalchemy models are called models)
+
+from sqlalchemy import Column,Integer,String
+from .database import Base
+
+# DataBase for Blogs
+class Blog(Base):
+    __tablename__="blogs collection"
+    id = Column(Integer,primary_key=True,index=True)
+    title = Column(String)
+    body = Column(String)
+
+# DataBase for Users
+class User(Base):
+    __tablename__="Users Info"
+    id = Column(Integer,primary_key=True,index=True)
+    name= Column(String)
+    email= Column(String)
+    password= Column(String)
