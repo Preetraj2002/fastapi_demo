6d0b0ec8b781721ebdabf67d483c8499133f9e9d
Preetraj <preetrajgupta2002@gmail.com>
2023-01-28 22:13:35 +0530

    adding user schemes

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,6 +1,23 @@
+# Response model also called response schemas
+# Data-driven classes
+# Pydantic is a data validation module
+# Pydantic models are called Schemas
+
+
 from pydantic import BaseModel
 
 class Blog(BaseModel):
     title:str
     body:str
 
+class ShowBlog(BaseModel):
+    title:str
+    body:str
+    class Config():
+        orm_mode=True
+
+
+class User(BaseModel):
+    name:str
+    email:str
+    password:str
