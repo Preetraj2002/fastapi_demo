65359b1f38edad5443cbcd6150730a91758c2981
Preetraj <preetrajgupta2002@gmail.com>
2023-02-01 13:54:44 +0530

    returning token

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter,Depends,HTTPException,status
 from sqlalchemy.orm import Session
 from .. import schemas,database,models
 from ..hashing import Hasher
+from ..JWTtoken import create_access_token
 
 router = APIRouter(
     tags=["Auth"],
@@ -14,9 +15,10 @@ def login(request:schemas.Login,db:Session=Depends(database.get_db)):
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Invalid Credentials !! User with id {request.username} not available in the database")
 
-    matching = Hasher.verify_password(request.password,user.password)
+    matching = Hasher.verify_password(request.password,user.password)       # (plain_pwd,hash_pwd)
 
     if not matching:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Incorrect Password")
-    return user
+    access_token = create_access_token(data={"sub":user.email})
+    return {'access_token': access_token,'token-type':"bearer"}
