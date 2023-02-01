eddb107c98cf845de2fe95748a1d70407768c63e
Preetraj <preetrajgupta2002@gmail.com>
2023-02-02 01:51:50 +0530

    adding verification layer in endpoints in blogs route

--- blog/routers/blogs.py
+++ blog/routers/blogs.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter,Depends,HTTPException,status
 from .. import schemas,models
 from sqlalchemy.orm import Session
 from ..database import get_db
+from ..OAuth2 import get_current_user
 
 
 
@@ -13,13 +14,9 @@ router = APIRouter(
 )
 
 
-@router.get('/',response_model=List[schemas.ShowBlog])
-def all(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
-
 @router.post("/",status_code=status.HTTP_201_CREATED)
-def create(request:schemas.Blog,db: Session = Depends(get_db)): # the Depends function converts the Session obj into pydantic thing
+def create(request:schemas.Blog,db: Session = Depends(get_db),current_user: schemas.User = Depends(get_current_user)):
+                                                                # the Depends function converts the Session obj into pydantic thing
                                                                 # request in a Blog obj (one of the schemas)
     '''creating blog with user passed title and body'''
 
@@ -31,14 +28,14 @@ def create(request:schemas.Blog,db: Session = Depends(get_db)): # the Depends fu
     # return f'creating blog with title:{request.title} and body:{request.body}'
 
 @router.get('/',response_model=List[schemas.ShowBlog])
-def all(db: Session = Depends(get_db)):
+def all(db: Session = Depends(get_db),current_user: schemas.User = Depends(get_current_user)):      # get current user applies lock to the endpoint
     blogs= db.query(models.Blog).all()              # query for passing an search request
                                                     # all() fetches all the items in the database
     return blogs
 
 
 @router.get('/{id}',status_code=200,response_model=schemas.ShowBlog)
-def show_blog(id,db: Session = Depends(get_db)):
+def show_blog(id,db: Session = Depends(get_db),current_user: schemas.User = Depends(get_current_user)):
     blog= db.query(models.Blog).filter(models.Blog.id==id).first()          # filter picks single entry from the database with a field like Blog.id
                                                                               # first() gives the sigle 1st entry found
     if blog == None:
@@ -49,7 +46,7 @@ def show_blog(id,db: Session = Depends(get_db)):
     return blog
 
 @router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT)        # 204 is bound to return no content
-def delete_blog(id,db : Session = Depends(get_db)):
+def delete_blog(id,db : Session = Depends(get_db),current_user: schemas.User = Depends(get_current_user)):
     blog=db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with id {id} is not available")
@@ -58,7 +55,7 @@ def delete_blog(id,db : Session = Depends(get_db)):
     return {"detail":"deleted successfully"}
 
 @router.put('/{id}',status_code=status.HTTP_202_ACCEPTED)     # update a existing post
-def update(id,request:schemas.Blog,db:Session= Depends(get_db)):
+def update(id,request:schemas.Blog,db:Session= Depends(get_db),current_user: schemas.User = Depends(get_current_user)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with {id} is not available")
